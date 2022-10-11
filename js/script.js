af59f400996831e953c6c5d678216001a22c9103
DeboraCocchi <cccdbr@gmail.com>
2022-10-11 17:44:22 +0200

    [modified]glacier-express-as-bg

--- js/script.js
+++ js/script.js
@@ -20,14 +20,10 @@ if(datiValidi){
 if(datiValidi){
   if(userAge < 18){
     discount = 0.2;
-    document.getElementById('sconto').innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-    </svg> Puoi beneficiare di uno sconto del ${discount*100}%!`
+    document.getElementById('sconto').innerHTML=`&#8594;  Puoi beneficiare di uno sconto del ${discount*100}%!`
   }else if(userAge >= 65){
     discount = 0.4;
-    document.getElementById('sconto').innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-  </svg> Puoi beneficiare di uno sconto del ${discount*100}% !`
+    document.getElementById('sconto').innerHTML=`&#8594; Puoi beneficiare di uno sconto del ${discount*100}% !`
   }else{
     discount = 0;
   } 
